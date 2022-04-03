414841e8f69dbabaf3552de3216c2c8fd390cd34
lenargasimov <lenargasimov@duck.com>
2022-04-04 00:44:50 +0300

    feat: added sheduler

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from datetime import datetime
+import time
 import os
 import telebot
 import pandas
+import schedule
 import config
+from multiprocessing import *
+from telebot import types
 
 bot = telebot.TeleBot(config.token)
 group_id = os.environ.get('GROUP_ID')
@@ -10,9 +14,32 @@ group_id = os.environ.get('GROUP_ID')
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    name = birthday_person["name"]
-    bot.send_message(group_id, f"С Днём Рождения {name}! 🥳🥳🥳👋👋👋🔈🔈🔈🍺🍺🍺🍻🍻🍻😃😃😃")
+
+def start_process():
+    p1 = Process(target=TimeSchedule.start_schedule, args=()).start()
+
+
+class TimeSchedule():
+    def start_schedule():
+        schedule.every().day.at("4:00").do(TimeSchedule.send_congratulations)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+
+    def send_congratulations():
+        data = pandas.read_csv("birthdays.csv")
+        birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+        if today_tuple in birthdays_dict:
+            birthday_person = birthdays_dict[today_tuple]
+            name = birthday_person["name"]
+            bot.send_message(group_id, f"С Днём Рождения {name}! 🥳🥳🥳👋👋👋🔈🔈🔈🍺🍺🍺🍻🍻🍻😃😃😃")
+
+
+if __name__ == '__main__':
+    start_process()
+    try:
+        bot.polling(none_stop=True)
+    except:
+        pass
