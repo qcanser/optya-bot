42aec5388bc60874ead5397df4aa27a92a335f57
lenargasimov <lenargasimov@duck.com>
2022-04-04 20:15:42 +0300

    refactor: changed group_id

--- bot.py
+++ bot.py
@@ -12,8 +12,7 @@ from time import mktime
 
 token = os.environ['BOT_API_TOKEN']
 bot = telebot.TeleBot(token)
-# group_id = os.environ['GROUP_ID']
-group_id = '-770664035'
+group_id = os.environ['GROUP_ID']
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
