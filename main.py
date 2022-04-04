85e5bb380d8aad715bb0f9a6b0d4f1d66d6e88e8
lenargasimov <lenargasimov@duck.com>
2022-04-04 13:54:39 +0300

    feat: added send_new_podcast

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import telebot
 import pandas
 import schedule
 import config
+import feedparser
+
 from multiprocessing import *
 from telebot import types
+
 
 bot = telebot.TeleBot(config.token)
 group_id = os.environ.get('GROUP_ID')
@@ -21,7 +24,8 @@ def start_process():
 
 class TimeSchedule():
     def start_schedule():
-        schedule.every().day.at("4:00").do(TimeSchedule.send_congratulations)
+        schedule.every().day.at("04:00").do(TimeSchedule.send_congratulations)
+        schedule.every().day.at("04:01").do(TimeSchedule.send_new_podcast)
 
         while True:
             schedule.run_pending()
@@ -35,6 +39,12 @@ class TimeSchedule():
             birthday_person = birthdays_dict[today_tuple]
             name = birthday_person["name"]
             bot.send_message(group_id, f"С Днём Рождения {name}! 🥳🥳🥳👋👋👋🔈🔈🔈🍺🍺🍺🍻🍻🍻😃😃😃")
+
+
+    def send_new_podcast():
+        d = feedparser.parse("https://promodj.com/strogonov-radioshow-technopolis/podcast.xml")
+        podcast_link = d.entries[0]['link']
+        bot.send_message(group_id, f'🔥🔥🔥💯💯💯👍👍👍💪💪💪🙏🙏🙏 \n Свежий эфир радио-шоу "ТЕХНОПОЛИС" \n {podcast_link}')
 
 
 if __name__ == '__main__':
--- promodj.py
+++
@@ -1,6 +0,0 @@
-import feedparser
-
-d = feedparser.parse("https://promodj.com/strogonov-radioshow-technopolis/podcast.xml")
-
-print(d.entries[0]['link'])
-print(d['entries'][0]['url'])
