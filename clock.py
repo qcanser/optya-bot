7aaae3dff721890dd637c140b7f425215063da4f
lenargasimov <lenargasimov@duck.com>
2022-04-05 13:58:23 +0300

    test build

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from bot import send_congratulations, send_new_podcast
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=4)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=11)
 def timed_job():
     send_congratulations()
     send_new_podcast()
