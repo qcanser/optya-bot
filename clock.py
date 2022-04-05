2b950ce3ee8dc455d835c005dbb540add336a1ef
lenargasimov <lenargasimov@duck.com>
2022-04-05 15:03:12 +0300

    feat: added timezone europe/moscow

--- clock.py
+++ clock.py
@@ -1,5 +1,5 @@
 # from apscheduler.schedulers.blocking import BlockingScheduler
-# from bot import send_congratulations, send_new_podcast
+from bot import send_congratulations, send_new_podcast
 
 # sched = BlockingScheduler()
 
@@ -14,12 +14,12 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-sched = BlockingScheduler()
+sched = BlockingScheduler(timezone="Europe/Moscow")
 
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=11, minute=35)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+sched.add_job(send_congratulations, send_new_podcast, 'cron', day_of_week='mon-sun', hour=15, minute=8)
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=11, minute=35)
+# def scheduled_job():
+#     print('This job is run every weekday at 5pm.')
 
 
 sched.start()
