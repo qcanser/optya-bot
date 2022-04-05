a2a735c022b1fad0b5be2c0ce1ec7e22f45b82d8
lenargasimov <lenargasimov@duck.com>
2022-04-05 15:19:04 +0300

    test build

--- clock.py
+++ clock.py
@@ -14,9 +14,9 @@ from bot import send_congratulations, send_new_podcast
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-sched = BlockingScheduler(timezone="Europe/Moscow")
+sched = BlockingScheduler()
 
-sched.add_job(send_congratulations, send_new_podcast, 'cron', day_of_week='mon-sun', hour=15, minute=17)
+sched.add_job(send_congratulations, send_new_podcast, 'cron', day_of_week='mon-sun', hour=12, minute=20)
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=11, minute=35)
 # def scheduled_job():
 #     print('This job is run every weekday at 5pm.')
