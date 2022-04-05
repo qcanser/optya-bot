e9cc3d0c6826fa0f138d71dab54ed30e3732bf3f
lenargasimov <lenargasimov@duck.com>
2022-04-05 14:27:14 +0300

    test build

--- clock.py
+++ clock.py
@@ -1,13 +1,25 @@
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# from bot import send_congratulations, send_new_podcast
+
+# sched = BlockingScheduler()
+
+
+# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=11)
+# def timed_job():
+#     send_congratulations()
+#     send_new_podcast()
+
+
+# sched.start()
+
 from apscheduler.schedulers.blocking import BlockingScheduler
-from bot import send_congratulations, send_new_podcast
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=11)
-def timed_job():
-    send_congratulations()
-    send_new_podcast()
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=11, minute=35)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
 
 
 sched.start()
