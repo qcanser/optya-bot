10d0c3fac07c15a4a5d69a00adb625c048b243cc
lenargasimov <lenargasimov@duck.com>
2022-04-05 15:26:41 +0300

    test build

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from bot import send_congratulations
 
 sched = BlockingScheduler()
-sched.add_job(send_congratulations, 'cron', day_of_week='mon-sun', hour=12, minute=25)
+sched.add_job(send_congratulations, 'cron', day_of_week='mon-sun', hour=12, minute=29)
 
 
 sched.start()
